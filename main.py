a47f6d5f2b3e1f7a919ea29be73e94ba3b1cd580
IrynaBarnych <iryna.barnych@gmail.com>
2023-10-30 20:46:01 +0200

    перше завдання

--- main.py
+++ main.py
@@ -1,33 +1,15 @@
-class Animal:
-    def __init__(self, name, age, color, weight):
+# Завдання 1
+
+class Student:
+    def __init__(self, name, age):
         self.name = name
         self.age = age
-        self.color = color
-        self.weight = weight
 
-    def breath(self):
-        print("дихає")
-    def move(self):
-        print("бігає")
+    def print_info(self):
+        print(f"Ім'я: {self.name}, Вік: {self.age}")
 
-class Dog(Animal):
-    def voice(self):
-        print("Гавкає!")
-class Cat(Animal):
-    ...
+
 
 # Створення екземпляра класу "Собака"
-my_bird = Animal("Вуді", 70, "синьо-білий", 120)
-print(my_bird.name)
-bobik = Dog("Бобініо", 3, "красівий", 60000)
-murchik = Cat("Мурчелло", 10, "срібний табі", 3700)
-print(murchik.color)#атрибут об'єкта
-bobik.weight = 42000
-bobik.height = 60
-print(bobik.height)
-print(bobik.weight)
-#методи
-bobik.breath()
-bobik.voice()
-murchik.move()
-
+student1 = Student("Іван", 20)
+student1.print_info()
